b3f46b6cd1f0a071600d3416ecf28dc9db49e861
Kateřina Šebová <sebovamail@gmail.com>
2023-12-01 20:19:48 +0100

    day 1, definitely not the most optimized

---
+++ day1.py
@@ -0,0 +1,35 @@
+with open('day1-data.txt') as f:
+    data = f.readlines()
+
+class numero:
+    def __init__(self, i,v):
+        self.index = i
+        self.value = v
+    def __lt__(self, other):
+        return self.index < other.index
+    
+final = 0
+
+numerinos = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+def findNumbers(line):
+    result = []
+    for idx, char in enumerate(line):
+        if char.isdigit(): 
+            result.append(numero(idx,int(char)))
+    for idx, i in enumerate(numerinos):
+        if line.find(i) != -1:
+            result.append(numero(line.find(i), idx+1))
+        if line.rfind(i) != -1:
+            if line.rfind(i) != result[-1].index:
+              result.append(numero(line.rfind(i), idx+1))
+    result.sort()
+    end = str(result[0].value) + str(result[-1].value)
+    return int(end) 
+
+
+for line in data:
+    final += findNumbers(line)
+            
+        
+print(final)
+
